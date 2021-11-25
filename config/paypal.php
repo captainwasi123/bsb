05ee959423d4755a30e681beac36266d43963ad5
captainwasi123 <captain.wasi@gmail.com>
2021-11-25 23:30:39 +0500

    live keys

--- config/paypal.php
+++ config/paypal.php
@@ -1,9 +1,9 @@
 <?php
 return [ 
-    'client_id' => 'AS-RupLShh6TSZsFDI0zfqtGNx_LFSnQs7oD8BTB6mOCyqp4-i1NLukR7uQTaw4qlPElRyluYFbiM8b2',
-	'secret' => 'ED3w3vTqCPC2bn1kFLxZrp_wC93DdTMo12Ww0Bedt2uEPrDxlqFKjEOucZVGq_iEmXe819nDa3ETbutW',
+    'client_id' => 'ATGh_fwguT48kFwIhZO_zQjeoHj-6FFRTNWSbvRT0pVxJ78MgHupXM6WCCxlwrlQB0VSu6oKVh1LjdS0',
+	'secret' => 'EKZOO8uMaYlls3hbZNf-4_3TtuxbkJvyi79y5xIUYiPrAeNlxn_rrvNHBovmV5tuxD0xiKKZUV3WvNr5',
     'settings' => array(
-        'mode' => 'sandbox',
+        'mode' => 'live',
         'http.ConnectionTimeOut' => 1000,
         'log.LogEnabled' => true,
         'log.FileName' => storage_path() . '/logs/paypal.log',
